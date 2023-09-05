859c179bf66760d238c220c0001dc50056de302b
Steve Lianoglou <slianoglou@mazetx.com>
2023-09-05 15:14:22 -0700

    Moves flm_def.ReactiveFacileDataStore to FacileAnalysisShine
    This included a dependency on FacileShine, which should not be in this package

--- R/flm_def-base.R
+++ R/flm_def-base.R
@@ -399,27 +399,6 @@ flm_def.FacileDataStore <- function(x, covariate, numer = NULL, denom = NULL,
           custom_key = custom_key, ...)
 }
 
-#' @export
-#' @rdname flm_def
-#' @importFrom FacileShine active_samples
-#' @importFrom FacileViz unselected
-flm_def.ReactiveFacileDataStore <- function(x, covariate, numer = NULL,
-                                            denom = NULL, batch = NULL,
-                                            block = NULL,
-                                            on_missing = c("warning", "error"),
-                                            ..., samples = active_samples(x),
-                                            custom_key = user(x)) {
-  samples <- collect(samples, n = Inf)
-  if (unselected(numer)) numer <- NULL
-  if (unselected(denom)) denom <- NULL
-  if (unselected(batch)) batch <- NULL
-  if (unselected(block)) block <- NULL
-
-  flm_def(samples, covariate = covariate, numer = numer, denom = denom,
-          batch = batch, block = block, on_missing = on_missing,
-          custom_key = custom_key, ...)
-}
-
 # redo =========================================================================
 
 #' @noRd
