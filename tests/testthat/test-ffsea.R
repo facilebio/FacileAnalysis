5e833eacdd1af026cd65d20203e6fda3788a0461
Steve Lianoglou <slianoglou@gmail.com>
2023-10-03 11:58:34 -0700

    test update

--- tests/testthat/test-ffsea.R
+++ tests/testthat/test-ffsea.R
@@ -1,29 +1,58 @@
 context("Test Feature Set Enrichment Analysis (ffsea)")
 
-if (!exists("FDS")) {
-  FDS <- FacileData::exampleFacileDataSet()
+if (!exists("afds")) {
+  afds <- FacileData::an_fds()
 }
 if (!exists("gdb")) {
-  gdb <- sparrow::getMSigGeneSetDb("h", "human", id.type = "entrez")
+  gdb <- sparrow::getMSigGeneSetDb("h", "human", id.type = "ensembl")
+}
+if (!exists("fsamples")) {
+  fsamples <- FacileData::some_samples(sparse = FALSE) |> FacileData::with_sample_covariates()
+}
+if (!exists("ssamples")) {
+  ssamples <- FacileData::some_samples(sparse = TRUE)
 }
 
-ttest.res <- FDS |>
-  FacileData::filter_samples(indication == "CRC") |>
-  flm_def(covariate = "sample_type",
-          numer = "tumor", denom = "normal", batch = "sex") |>
+# if (!exists("FDS")) {
+#   FDS <- FacileData::exampleFacileDataSet()
+# }
+# 
+# if (!exists("gdb")) {
+#   gdb <- sparrow::getMSigGeneSetDb("h", "human", id.type = "entrez")
+# }
+
+# ttest.res <- FDS |>
+#   FacileData::filter_samples(indication == "CRC") |>
+#   flm_def(covariate = "sample_type",
+#           numer = "tumor", denom = "normal", batch = "sex") |>
+#   fdge(method = "voom")
+# anova.res <- FDS |>
+#   FacileData::filter_samples(indication == "CRC", sample_type == "tumor") |>
+#   flm_def(covariate = "stage", batch = "sex") |>
+#   fdge(method = "voom")
+# 
+# pca.res <- fpca(samples(anova.res))
+
+numer. <- "AKI__IMM"
+denom. <- "HCKD__IMM"
+
+ttest.res <- fsamples |> 
+  flm_def(covariate = "group", numer = numer., denom = denom.) |> 
   fdge(method = "voom")
-anova.res <- FDS |>
-  FacileData::filter_samples(indication == "CRC", sample_type == "tumor") |>
-  flm_def(covariate = "stage", batch = "sex") |>
-  fdge(method = "voom")
+
+anova.res <- suppressWarnings({
+  fsamples |> 
+    filter(cell_abbrev == "IMM") |> 
+    flm_def(covariate = "group") |> 
+    fdge(method = "voom")
+})
 
 pca.res <- fpca(samples(anova.res))
 
 test_that("ffsea.FacileAnalysisResult transfers all feature-level statistics to mulitGSEA result", {
   # This is really testing that the multGSEA hack currently in place that allows
   # us to pass in meta feature information and store it in the @logFC works.
-
-  facile.gsea <- ffsea(ttest.res, gdb, methods = "cameraPR")
+  facile.gsea <- ffsea(ttest.res, gdb, methods = "cameraPR", rank_by = "t")
 
   # are the logFC, t-statistics, pvals, padj for each gene transferred
   # successfully?
@@ -53,9 +82,10 @@ test_that("cameraPR call through ffsea works like sparrow::seas()", {
 
   # GSEA the "traditional" sparrow way
   vm <- biocbox(ttest.res)
-  mgres <- sparrow::seas(vm, gdb, "cameraPR",
-                         design = vm$design, contrast = c(-1, 1, 0),
-                         score.by = "logFC")
+  mgres <- sparrow::seas(
+    vm, gdb, "cameraPR", design = vm$design, 
+    contrast = ttest.res$params$model_def$contrast,
+    score.by = "logFC")
 
   mgres.cameraPR <- mgres |>
     sparrow::result("cameraPR") |>
@@ -74,10 +104,10 @@ test_that("overrepresentation analysis (ora) works with ttest result", {
     tidy() |>
     mutate(significant = padj <= 0.10)
   mgres <- sparrow::ora(input, gdb, selected = "significant",
-                        feature.bias = "effective_length")
+                        feature.bias = "AveExpr")
   mgres$name <- sub(".*;;", "", mgres$Pathway)
   facile.gsea <- ffsea(ttest.res, gdb, method = "ora",
-                       biased_by = "effective_length",
+                       biased_by = "AveExpr",
                        max_padj = 0.10, min_logFC = 0)
   fres <- tidy(facile.gsea)
   expect_equal(fres$name, mgres$name)
@@ -89,11 +119,11 @@ test_that("ffsea(anova_result) runs enrichment test", {
     tidy() |>
     mutate(significant = padj <= 0.2)
   mgres <- sparrow::ora(astats, gdb, selected = "significant",
-                        feature.bias = "effective_length")
+                        feature.bias = "AveExpr")
   mgres$name <- sub(".*;;", "", mgres$Pathway)
 
   facile.gsea <- ffsea(anova.res, gdb, "ora", max_padj = 0.2,
-                       biased_by = "effective_length")
+                       biased_by = "AveExpr")
   fres <- tidy(facile.gsea) |> select(collection, name, pval, N, n, n.drawn)
 
   expect_equal(fres$name, mgres$name)
